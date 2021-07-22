c9d70bbf5ea02df2634397758daf3601c5a94870
heesu-ahn <anhesu4@gmail.com>
2021-07-22 18:26:22 +0900

    [MOD] 호스트 등록 기능 개선 - 테스트 코드.py 추가 - 호스트 등록 수정 조회 기능 추가 및 개선 - 불필요한 select_related 제거 및 profile_url을   프론트엔드에서 보내주는 데이터로 저장함

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,97 @@
-from django.test import TestCase
+import json
+from django.http import response
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from unittest.mock import patch, MagicMock
+from users.models import Host, User
+
+class HostTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id          = 1,
+            kakao_id    = 1,
+            name        = 'anhesu',
+            profile_url = 'testurl'
+        )
+        User.objects.create(
+            id          = 2,
+            kakao_id    = 1,
+            name        = 'anhesu',
+            profile_url = 'testurl'
+        )
+        Host.objects.create (
+            nickname    = "test",
+            user_id     = 1,
+            profile_url = 'testurl'
+        )
+
+    def test_hostview_post_success(self):
+        client = Client()
+        host   = {
+            'nickname'   : 'anhesu',
+            'profile_url': 'testurl',
+            'user_id'    : 2
+        }
+        response = client.post('/users/host/2',json.dumps(host),content_type='application/json')
+        self.assertEqual(response.status_code,201)
+
+    def test_hostview_post_keyerror(self):
+        client = Client()
+        host   = {
+            'nickname'   : 'anhesu',
+            'profileurl': 'testurl',
+            'userid'    : 2
+        }
+        response = client.post('/users/host/2',json.dumps(host),content_type='application/json')
+        self.assertEqual(response.status_code,404)
+        self.assertEqual(response.json(),{"MESSAGE":"KEY_ERROR"})
+
+    def test_hostview_post_user_notexists(self):
+        client = Client()
+        host   = {
+            'nicknamee'   : 'anhesu',
+            'profileurll': 'testurl',
+            'useridd'    : 20
+        }
+        response = client.post('/users/host/2',json.dumps(host),content_type='application/json')
+        self.assertEqual(response.status_code,404)
+
+    def test_hostview_get(self):
+        client   = Client()
+        response = client.get('/users/host/1',content_type='application/json')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.json(),{'id': 1, 'user_id': 1, 'nickname': 'test', 'profile_url': 'testurl'})
+
+    def test_hostview_get_host_notexists(self):
+        client   = Client()
+        response = client.get('/users/host/10',content_type='application/json')
+        self.assertEqual(response.status_code,404)
+
+    def test_hostview_patch_success(self):
+        client = Client()
+        host   = {
+            'nickname'   : 'modifynickname',
+            'profile_url': 'modifyurl'
+        }
+        response = client.patch('/users/host/1',json.dumps(host),content_type='application/json')
+        self.assertEqual(response.status_code,201)
+        self.assertEqual(response.json(),{"MESSAGE":"SUCCESS"})
+
+    def test_hostview_patch_keyerror(self):
+        client = Client()
+        host   = {
+            'nicknamee'   : 'modifynickname',
+            'profileurll': 'modifyurl'
+        }
+        response = client.patch('/users/host/1',json.dumps(host),content_type='application/json')
+        self.assertEqual(response.status_code,404)
+
+    def test_hostview_post_host_notexists(self):
+        client = Client()
+        host   = {
+            'nickname'   : 'modifynickname',
+            'profileurl': 'modifyurl'
+        }
+        response = client.patch('/users/host/1',json.dumps(host),content_type='application/json')
+        self.assertEqual(response.status_code,404)
--- users/views.py
+++ users/views.py
@@ -6,12 +6,33 @@ from django.http  import JsonResponse
 from users.models import User, Host
 
 class HostView(View):
+
+    def get(self, request, user_id):
+        try:
+            hostuser = Host.objects.get(user_id = user_id)
+            result   = {
+                'id'            : hostuser.id,
+                'user_id'       : hostuser.user_id,
+                'nickname'      : hostuser.nickname,
+                'profile_url'   : hostuser.profile_url
+            }
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
+
+        except Host.DoesNotExist:
+            return JsonResponse({"MESSAGE": "INVALID_HOST"}, status=404)
+        
+        return JsonResponse(result, status=200)       
+
     def post(self, request, user_id):
         try:
             data        = json.loads(request.body)
+            print(user_id)
             user        = User.objects.get(id = user_id)
+            print(user)
             nickname    = data['nickname']
-            profile_url = user.profile_url
+            profile_url = data['profile_url']
             
             if Host.objects.filter(user_id = user.id).exists() :
                 return JsonResponse({"MESSAGE": "DUPLE_USER"}, status=404)
@@ -31,20 +52,20 @@ class HostView(View):
         
         return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
 
-    def get(self, request, user_id):
-        try:
-            hostuser = Host.objects.select_related('user').get(user_id = user_id)
-            result   = {
-                'id'            : hostuser.id,
-                'user_id'       : hostuser.user_id,
-                'nickname'      : hostuser.nickname,
-                'profile_url'   : hostuser.profile_url
-            }
+    def patch(self, request, user_id):
+            try:
+                data             = json.loads(request.body)
+                nickname         = data['nickname']
+                profile_url      = data['profile_url']
+                host             = Host.objects.get(user_id = user_id)
+                host.nickname    = nickname
+                host.profile_url = profile_url
+                host.save()
 
-        except KeyError:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
+            except KeyError:
+                return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
 
-        except User.DoesNotExist:
-            return JsonResponse({"MESSAGE": "INVALID_USER"}, status=404)
-        
-        return JsonResponse(result, status=200)       
+            except Host.DoesNotExist:
+                return JsonResponse({"MESSAGE": "INVALID_HOST"}, status=404)
+            
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
