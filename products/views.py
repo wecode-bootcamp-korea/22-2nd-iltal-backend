9a8ef274a29a2c6f7d7f565eb0cc11855c991246
heesu-ahn <anhesu4@gmail.com>
2021-07-27 12:49:26 +0900

    aa

--- products/views.py
+++ products/views.py
@@ -11,13 +11,13 @@ from products.models import Product
 class ProductView(View):
     def post(self, request):
         try:
-            
+            print(request.FILES.get('background_url'))
             Product.objects.create (
                 title           =  request.POST.get('title'),
                 region          =  request.POST.get('region'),
                 price           =  request.POST.get('price'),
                 is_group        =  request.POST.get('is_group'),
-                background_url  =  core.views.upload_data(request.POST.get('background_url')),
+                background_url  =  core.views.upload_data(request.FILES.get('background_url')),
                 is_deleted      =  False,
                 host_id         =  request.POST.get('host_id'),
                 subcategory_id  =  request.POST.get('subcategory_id')
